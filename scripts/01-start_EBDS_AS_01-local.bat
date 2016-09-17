74288f90e610bdce2fdf09c9a9a5c91edb973231
viragtripathi <virag.tripathi@gmail.com>
2016-09-17 16:14:50 -0400

    updated project and scripts
    chnaged persistence type to none etc.

--- scripts/01-start_EBDS_AS_01-local.bat
+++ scripts/01-start_EBDS_AS_01-local.bat
@@ -1,3 +1,3 @@
 set PATH=%AS_HOME%/lib;%PATH%
 set AS_LOG_DIR="%AS_HOME%/logs"
-%AS_HOME%/bin/as-agent -metaspace "EnterpriseBagDataStoreMS" -discovery "tcp://127.0.0.1:8001;:8002" -listen "tcp://127.0.0.1:8001" -log "%AS_LOG_DIR%/EBDS_ASAGENT_01.log" -log_debug "4" -log_limit "10000000" -log_count "10" -member_name "EBDS_ASAGENT_01" > EBDS_ASAGENT_01.out 2>&1
+%AS_HOME%/bin/as-agent -metaspace "EnterpriseBagDataStoreMS" -discovery "tcp://127.0.0.1:8001;127.0.0.1:8002" -listen "tcp://127.0.0.1:8001" -log "%AS_LOG_DIR%/EBDS_ASAGENT_01.log" -log_debug "4" -log_limit "10000000" -log_count "10" -member_name "EBDS_ASAGENT_01" > EBDS_ASAGENT_01.out 2>&1
